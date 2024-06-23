d64142150529bc66614b9db6325432a37e7c5491
Kazaki-sama <maharjansubham7@gmail.com>
2024-06-23 20:40:01 +0545

    update photo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,30 @@ export default function Home() {
         <p>
           I am Subham Maharjan.
         </p>
-        
+        <hr/>
+        <div>
+        <b>SKILLS </b>
+        <hr/>
+          <ul>
+            <li>Database-management: Capable uf using Excel and Sql </li>
+            <li> Programming: making game in lua and know other language like C, python,javascript and reactjs as a framework  </li>
+            <li>Problem-Solving: Think and look at the problem in a bigger picture</li>
+            <li>Designing: photoshop, figma and lightroom are key component to desgin</li>
+            </ul>  
+        </div>
+        <div>
+          <b>EXPERIENCE </b>
+          <hr/>
+            Seson, Basundhara 
+              <ul>
+                <li>Social media coordinator </li>
+                <li>Social media post designer</li>
+              </ul>
+        </div>
+        <div>
+            <b>PROJECTS</b>
+            
+        </div>
       </div>
     </main>
   );
