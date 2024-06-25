5d7e6466f2a904555099942a1ee3f6a9469ebae1
Kazaki-sama <maharjansubham7@gmail.com>
2024-06-25 12:33:47 +0545

    w

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          I am Subham Maharjan.
+          Subham Maharjan.
         </p>
         <hr/>
         <div>
