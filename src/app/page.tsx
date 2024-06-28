a855cffee22a5abae9e2c3e757856aa287cd8409
Kazaki-sama <maharjansubham7@gmail.com>
2024-06-28 17:10:03 +0545

    revert

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,32 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          <h1><b>JEASON GAY</b></h1>
-          <h1><b>MANAV GAY</b></h1>
-          <h1><b>prasan GAY</b></h1>
-          <h1><b>AMIR GAY</b></h1>
+      <p>
+          Subham Maharjan.
         </p>
         <hr/>
-        
+        <div>
+        <b>SKILLS </b>
+        <hr/>
+          <ul>
+            <li>Database-management: Capable uf using Excel and Sql </li>
+            <li> Programming: making game in lua and know other language like C, python,javascript and reactjs as a framework  </li>
+            <li>Problem-Solving: Think and look at the problem in a bigger picture</li>
+            <li>Designing: photoshop, figma and lightroom are key component to desgin</li>
+            </ul>  
+        </div>
+        <div>
+          <b>EXPERIENCE </b>
+          <hr/>
+            Seson, Basundhara 
+              <ul>
+                <li>Social media coordinator </li>
+                <li>Social media post designer</li>
+              </ul>
+        </div>
+        <div>
+            <b>PROJECTS</b>
+      </div>
       </div>
     </main>
   );
