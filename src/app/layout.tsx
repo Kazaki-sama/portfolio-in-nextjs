e31f76ed6a2a3944e1a7e8f6012ad9e82c235417
Kazaki-sama <maharjansubham7@gmail.com>
2024-06-29 10:10:59 +0545

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Link from "next/link";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -15,7 +15,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      
+      <body className={inter.className}>
+      <div>
+        <ul >
+         <li> Subham Maharjan / Designer / Web Developer </li>
+        
+         <li> <Link href={"/"}>About me</Link></li>
+         <li> <Link href="/resume">Resume</Link></li>
+         <li> <Link href="/Project">Project</Link></li>
+          </ul>
+          </div>
+        <hr/>
+        
+        {children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-      <p>
-          Subham Maharjan.
-        </p>
-        <hr/>
-        <div>
-        <b>SKILLS </b>
-        <hr/>
-          <ul>
-            <li>Database-management: Capable uf using Excel and Sql </li>
-            <li> Programming: making game in lua and know other language like C, python,javascript and reactjs as a framework  </li>
-            <li>Problem-Solving: Think and look at the problem in a bigger picture</li>
-            <li>Designing: photoshop, figma and lightroom are key component to desgin</li>
-            </ul>  
-        </div>
-        <div>
-          <b>EXPERIENCE </b>
-          <hr/>
-            Seson, Basundhara 
-              <ul>
-                <li>Social media coordinator </li>
-                <li>Social media post designer</li>
-              </ul>
-        </div>
-        <div>
-            <b>PROJECTS</b>
-      </div>
+       
+        
+      
       </div>
     </main>
   );
