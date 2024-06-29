578cab95b7068c2edaec1950b1f3f1dee9c39c5b
Kazaki-sama <maharjansubham7@gmail.com>
2024-06-29 10:11:10 +0545

    navbar

---
+++ src/app/Project/page.tsx
@@ -0,0 +1,16 @@
+import { GetServerSideProps } from 'next';
+import styles from '../page.module.css';
+const Project = () => {
+    return (
+
+    <div className={styles.main}>
+        <div className={styles.description}>
+        Enter
+        </div>
+    </div>
+    );
+}
+
+
+
+export default Project
---
+++ src/app/resume/page.tsx
@@ -0,0 +1,15 @@
+import { GetServerSideProps } from 'next';
+import styles from '../page.module.css';
+const resume = () => {
+    return (
+        <div className={styles.main}>
+            <div className={styles.description}>
+            Enter
+            </div>
+        </div>
+    );
+}
+
+
+
+export default resume
